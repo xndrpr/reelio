c3595c8bce7c8d085a9e6b6167b5ef9273780770
Александр Прокопенко <velaton618@gmail.com>
2024-08-31 13:27:02 +0800

    Init + colors + emotion

---
+++ src/app/styled.ts
@@ -0,0 +1,4 @@
+"use client";
+
+import styled from "@emotion/styled";
+
---
+++ src/variables/index.ts
@@ -0,0 +1,12 @@
+export const bgColor = "#0F1118";
+export const fgColor = "#DBDBDB";
+
+export const darkPurple = "#1C1C26";
+export const mediumPurple = "#232331";
+export const lightPurple = "#3B4785";
+
+export const lightGrey = "#888888";
+export const darkGrey = "#444444";
+
+export const imdbColor = "#C49F1D";
+export const kpColor = "#CA581F";
