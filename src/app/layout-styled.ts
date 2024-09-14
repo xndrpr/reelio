c8171d3c936dc47999b2c89b5d96dbfc706547aa
Александр Прокопенко <velaton618@gmail.com>
2024-09-14 17:56:51 +0800

    remove global

--- src/app/layout-styled.ts
+++ src/app/layout-styled.ts
@@ -1,31 +1,7 @@
 "use client";
 
-import {
-  bgColor,
-  breakPoint1,
-  breakPoint2,
-  breakPoint3,
-  fgColor,
-} from "@/shared/variables";
+import { breakPoint1, breakPoint2, breakPoint3 } from "@/shared/variables";
 import styled from "@emotion/styled";
-import { createGlobalStyle } from "styled-components";
-
-export const GlobalStyles = createGlobalStyle`
-  body,
-  html {
-    font-family: 'Montserrat', sans-serif;
-    background-color: ${bgColor};
-    color: ${fgColor};
-  }
-
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-
-    -webkit-tap-highlight-color: transparent;
-  }
-`;
 
 export const ContentWrapper = styled.main`
   padding: 40px 120px;
