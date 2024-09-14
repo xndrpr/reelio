c2ae154808a531e89db834ef33b8da93049cf3d3
Александр Прокопенко <velaton618@gmail.com>
2024-09-15 00:32:22 +0800

    remove console log

--- src/shared/components/search-bar/index.tsx
+++ src/shared/components/search-bar/index.tsx
@@ -20,10 +20,8 @@ export const SearchBar = ({ state }: Props) => {
   }, [data, state]);
 
   useEffect(() => {
-    console.log(state.value.length);
     if (state.value.length === 0) {
       state.setResult([]);
-      console.log(state.result);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [state.value]);
