a445228096efa8b257073f235734e2865211b890
Александр Прокопенко <velaton618@gmail.com>
2024-09-14 22:05:01 +0800

    width: 100%

--- src/shared/components/kino-box/styled.ts
+++ src/shared/components/kino-box/styled.ts
@@ -7,21 +7,9 @@ import {
 import styled from "@emotion/styled";
 
 export const Container = styled.div`
-  width: 75%;
+  width: 100%;
   aspect-ratio: 16/9;
 
   border: 4px solid ${darkPurple};
   border-radius: 4px;
-
-  @media (max-width: ${breakPoint1}px) {
-    width: 80%;
-  }
-
-  @media (max-width: ${breakPoint2}px) {
-    width: 85%;
-  }
-
-  @media (max-width: ${breakPoint3}px) {
-    width: 90%;
-  }
 `;
