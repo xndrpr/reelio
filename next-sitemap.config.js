75a85f27336a8556207d7a81f29745db54eacf47
Александр Прокопенко <velaton618@gmail.com>
2024-09-19 21:05:01 +0800

    metadata

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://example.com',
+  siteUrl: process.env.NEXT_PUBLIC_METADATA_BASE_URL || 'https://example.com',
   generateRobotsTxt: true,
   sitemapSize: 7000
 }
