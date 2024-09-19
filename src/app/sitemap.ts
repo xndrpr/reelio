5691beeb7004eae513ed8db0f128b9ce269e53c3
Александр Прокопенко <velaton618@gmail.com>
2024-09-19 22:08:45 +0800

    anime tv

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -83,6 +83,90 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     );
   }
 
+  for (let i = 1; i <= 500; i++) {
+    console.log(`${i} / 500`);
+    const res = await fetch(
+      `${process.env.API_URL}/movies?offset=${i}&type=${2}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          secret: `${process.env.SECRET}`,
+        },
+        next: { revalidate: 60 * 30 },
+      }
+    );
+    const data = await res.json();
+
+    pages.push(
+      ...data?.data?.map(
+        (doc: any) =>
+          doc?.id && {
+            url: `${process.env.NEXT_PUBLIC_BASE_URL}/${
+              doc.seasons_count && doc.seasons_count > 0 ? "tv" : "movie"
+            }/${doc?.id}`,
+            lastModified: new Date(),
+            changeFrequency: "daily",
+            priority: 0.5,
+          }
+      )
+    );
+    pages.push(
+      ...data?.data?.map(
+        (doc: any) =>
+          doc?.id && {
+            url: `${process.env.NEXT_PUBLIC_BASE_URL}/${
+              doc.seasons_count && doc.seasons_count > 0 ? "tv" : "movie"
+            }/${doc?.id}/about`,
+            lastModified: new Date(),
+            changeFrequency: "daily",
+            priority: 0.5,
+          }
+      )
+    );
+  }
+
+  for (let i = 1; i <= 500; i++) {
+    console.log(`${i} / 500`);
+    const res = await fetch(
+      `${process.env.API_URL}/movies?offset=${i}&type=${3}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          secret: `${process.env.SECRET}`,
+        },
+        next: { revalidate: 60 * 30 },
+      }
+    );
+    const data = await res.json();
+
+    pages.push(
+      ...data?.data?.map(
+        (doc: any) =>
+          doc?.id && {
+            url: `${process.env.NEXT_PUBLIC_BASE_URL}/${
+              doc.seasons_count && doc.seasons_count > 0 ? "tv" : "movie"
+            }/${doc?.id}`,
+            lastModified: new Date(),
+            changeFrequency: "daily",
+            priority: 0.5,
+          }
+      )
+    );
+    pages.push(
+      ...data?.data?.map(
+        (doc: any) =>
+          doc?.id && {
+            url: `${process.env.NEXT_PUBLIC_BASE_URL}/${
+              doc.seasons_count && doc.seasons_count > 0 ? "tv" : "movie"
+            }/${doc?.id}/about`,
+            lastModified: new Date(),
+            changeFrequency: "daily",
+            priority: 0.5,
+          }
+      )
+    );
+  }
+
   return [
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
